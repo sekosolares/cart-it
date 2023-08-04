edd0836e253f5ddf0d9631bc1aec53864cfcb1b5
Axel J. Solares <axel.solares.g@gmail.com>
2023-08-04 07:51:10 -0600

    [Add]: New Routes
    Add routes to manage filter by category through an API request

--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
 export const CustomRoutes = {
   HOME: '/',
+  FILTER_CATEGORY: '/:categ',
   MY_ACCOUNT: '/my-account',
-  MY_ORDER: '/my-order',
+  MY_ORDER: '/my-order/:id',
   MY_ORDERS: '/my-orders',
-  SIGN_IN: '/sign-in'
+  SIGN_IN: '/sign-in',
+  MY_ORDERS_LAST: '/my-orders/last'
 }
