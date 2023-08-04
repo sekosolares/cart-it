08966b63371ad059f21ee5648c468339472a65a8
Axel J. Solares <axel.solares.g@gmail.com>
2023-08-04 07:58:52 -0600

    [Feat]: filtering by title and category

--- src/contexts/productContext.jsx
+++ src/contexts/productContext.jsx
@@ -1,6 +1,8 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 export const ProductsContext = createContext();
+
+const STORE_API = "https://fakestoreapi.com/products";
 
 // eslint-disable-next-line react/prop-types
 export function ProductsProvider({ children }) {
@@ -20,6 +22,40 @@ export function ProductsProvider({ children }) {
   // Shopping Cart data
   const [cartProducts, setCartProducts] = useState([]);
 
+  // My Order
+  const [myOrder, setMyOrder] = useState([]);
+
+  // Get products
+  const [products, setProducts] = useState([]);
+
+  // Filter products
+  const [filterTitle, setFilterTitle] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState(products);
+
+  useEffect(() => {
+    fetch(STORE_API)
+    .then(response => response.json())
+    .then(data => setProducts([...products, ...data]))
+    .catch(err => console.info(`Ocurrio un error: ${JSON.stringify(err, null, 2)}`));
+  }, []);
+
+  // filter by category
+  const [category, setCategory] = useState('');
+
+  useEffect(() => {
+    if(category) {
+      fetch(`https://fakestoreapi.com/products/category/${category}`)
+      .then(res => res.json())
+      .then(json => setFilteredProducts(json))
+      .catch(err => console.info(`Ocurrio un error al filtrar por categorias: ${JSON.stringify(err, null, 2)}`));
+    }
+  }, [category]);
+
+  useEffect(() => {
+    const filteredProducts = products.filter(prod => prod.title.toUpperCase().includes(filterTitle.toUpperCase()));
+    setFilteredProducts(filteredProducts);
+  }, [filterTitle, products]);
+
   const valueStore = {
     cartCounter,
     setCartCounter,
@@ -32,7 +68,16 @@ export function ProductsProvider({ children }) {
     setCartProducts,
     openCheckout,
     closeCheckout,
-    isCheckoutOpen
+    isCheckoutOpen,
+    myOrder,
+    setMyOrder,
+    products,
+    setProducts,
+    filterTitle,
+    setFilterTitle,
+    filteredProducts,
+    setFilteredProducts,
+    setCategory
   };
   return (
     <ProductsContext.Provider value={valueStore}>
